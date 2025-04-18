a3724afdbdc12f4fc8a797fde4e6f8d96c719085
Jms Dnns <jdennis@gmail.com>
2025-04-17 21:37:52 -0400

    Added comments to explain each member of env

--- src/newnoise/aws/env.py
+++ src/newnoise/aws/env.py
@@ -1,11 +1,16 @@
 import os
 
+# Default location for this module's data
 NOISES_ROOT = "./noises/aws"
+
+# Default location for data cache, eg. the sqlite database
 NOISES_DB = os.path.join(NOISES_ROOT, "cache.db")
-NOISES_CSV = os.path.join(NOISES_ROOT, "pro_ducks.csv")
 
+# Default location for CSV produced at end of this module's processes
+NOISES_CSV = os.path.join(NOISES_ROOT, "products.csv")
+
+# Path to AWS host that supplies the pricing data
 PRICE_API = "https://pricing.us-east-1.amazonaws.com"
-PRICE_ROOT = "/offers/v1.0/aws/index.json"
 
-# NO IDEA what this is or where it comes from
-SPOT_API = "https://website.spot.ec2.aws.a2z.com/spot.js"
+# HTTP path to file that stores the root of the pricing tree
+PRICE_ROOT = "/offers/v1.0/aws/index.json"
